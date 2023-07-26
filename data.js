e8ba66ab925429311fca90c27a372bc3e1f42989
Nick Teets <nickteets@gmail.com>
2023-07-26 11:37:05 -0600

    init commit

---
+++ data.js
@@ -0,0 +1,155 @@
+module.exports = {
+    deathValley: {
+      location: 'Death Valley, California, USA',
+      announcements: {
+        warning: false,
+        message: ''
+      },
+      forecast: [
+        {
+          "day": "Monday",
+          "high": 96,
+          "low": 60,
+          "condition": "Partly Cloudy"
+        },
+        {
+          "day": "Tuesday",
+          "high": 89,
+          "low": 62,
+          "condition": "Sunny"
+        },
+        {
+          "day": "Wednesday",
+          "high": 88,
+          "low": 58,
+          "condition": "Clear"
+        },
+        {
+          "day": "Thursday",
+          "high": 92,
+          "low": 61,
+          "condition": "Mostly Sunny"
+        },
+        {
+          "day": "Friday",
+          "high": 95,
+          "low": 65,
+          "condition": "Partly Cloudy"
+        },
+        {
+          "day": "Saturday",
+          "high": 91,
+          "low": 63,
+          "condition": "Partly Cloudy"
+        },
+        {
+          "day": "Sunday",
+          "high": 90,
+          "low": 64,
+          "condition": "Partly Cloudy"
+        }
+      ],
+    },
+    mawsynram: {
+      location: 'Mawsyram, Meghalaya, India',
+      announcements: {
+        warning: true,
+        message: 'Severe Storm Warning'
+      },
+      forecast: [
+        {
+          "day": "Monday",
+          "high": 22,
+          "low": 16,
+          "condition": "Rainy"
+        },
+        {
+          "day": "Tuesday",
+          "high": 20,
+          "low": 14,
+          "condition": "Thunderstorms"
+        },
+        {
+          "day": "Wednesday",
+          "high": 19,
+          "low": 13,
+          "condition": "Rainy"
+        },
+        {
+          "day": "Thursday",
+          "high": 21,
+          "low": 15,
+          "condition": "Showers"
+        },
+        {
+          "day": "Friday",
+          "high": 20,
+          "low": 14,
+          "condition": "Cloudy with Rain"
+        },
+        {
+          "day": "Saturday",
+          "high": 22,
+          "low": 16,
+          "condition": "Rainy"
+        },
+        {
+          "day": "Sunday",
+          "high": 21,
+          "low": 15,
+          "condition": "Rainy"
+        }
+      ]
+    },
+    denali: {
+      location: 'Denali, Alaska, USA',
+      announcements: {
+        warning: false,
+        message: ''
+      },
+      forecast: [
+        {
+          "day": "Monday",
+          "high": 28,
+          "low": 15,
+          "condition": "Snowy"
+        },
+        {
+          "day": "Tuesday",
+          "high": 30,
+          "low": 18,
+          "condition": "Cloudy"
+        },
+        {
+          "day": "Wednesday",
+          "high": 24,
+          "low": 12,
+          "condition": "Snow Showers"
+        },
+        {
+          "day": "Thursday",
+          "high": 20,
+          "low": 9,
+          "condition": "Snow"
+        },
+        {
+          "day": "Friday",
+          "high": 22,
+          "low": 11,
+          "condition": "Partly Cloudy"
+        },
+        {
+          "day": "Saturday",
+          "high": 25,
+          "low": 13,
+          "condition": "Mostly Cloudy"
+        },
+        {
+          "day": "Sunday",
+          "high": 23,
+          "low": 10,
+          "condition": "Snowy"
+        }
+      ]   
+    },
+  }
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3001;
+const app = express();
+const { deathValley, mawsynram, denali } = require('./data.js');
+
+app.locals = {
+  title: 'Weathrly Network (Requests)',
+  deathValley,
+  mawsynram,
+  denali
+}
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/api/v1/death-valley', (req, res) => {
+  res.status(200).json(app.locals.deathValley);
+});
+
+app.get('/api/v1/mawsynram', (req, res) => {
+  res.status(200).json(app.locals.mawsynram);
+});
+
+app.get('/api/v1/denali', (req, res) => {
+  res.status(200).json(app.locals.denali);
+});
+
+app.post('/api/v1/:location',(req, res) => {
+  if (req.params.location === 'death-valley') {
+    app.locals.deathValley.announcements = {
+      warning: true,
+      message: req.body.message
+    }
+  } else {
+    app.locals[req.params.location].announcements = {
+      warning: true,
+      message: req.body.message
+    }
+  }
+  res.status(201)
+})
+
+app.delete('/api/v1/:location', (req, res) => {
+  if (req.params.location === 'death-valley') {
+    app.locals.deathValley.announcements = {
+      warning: false,
+      message: ''
+    }
+  } else {
+    app.locals[req.params.location].announcements = {
+      warning: false,
+      message: ''
+    }
+  }
+  res.status(200)
+})
+
+app.listen(port, () => {
+  console.log(`${app.locals.title} is now running on http://localhost:${port} !`)
+});
